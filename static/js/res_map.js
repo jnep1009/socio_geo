dc4a9b33321fdb292df22133083c6abb5f6ca6e9
Thana-On Punkasem <JNEP@MacBook-Pro.local>
2015-12-14 13:30:44 -0600

    Updated map Legend

--- static/js/res_map.js
+++ static/js/res_map.js
@@ -15,18 +15,18 @@ define(['jquery', 'mapbox', 'highstock', 'd3', 'd3_plus', 'arc'], function ($, _
         return this._div;
     };
     info.update = function () {
-        this._div.innerHTML = ( "<i style='color:#fff800' class='fa fa-circle'></i>" + "  Shopping" +
-            "<br><i style='color:#ed1b3f' class='fa fa-circle'></i>" + "  Dining" +
-            "<br><i style='color:#33ff05' class='fa fa-circle'></i>" + "  Health Care" +
-            "<br><i style='color:#009cdd' class='fa fa-circle'></i>" + "Recreation" +
+        this._div.innerHTML = ( "<i style='color:#fff800' class='fa fa-circle'></i>" + "   Shopping" +
+            "<br><i style='color:#ed1b3f' class='fa fa-circle'></i>" + "   Dining" +
+            "<br><i style='color:#33ff05' class='fa fa-circle'></i>" + "   Health Care" +
+            "<br><i style='color:#009cdd' class='fa fa-circle'></i>" + "   Recreation" +
             "<br><b>Income</b>" +
-            "<br><i style='color:#e9bdc6'>-</i>" + "< 20000 USD" +
-            "<br><i style='color:#ed80ce'>-</i>" + "20000 - 34999 USD" +
-            "<br><i style='color:#c7a7fa'>-</i>" + "35000 - 49999 USD" +
-            "<br><i style='color:#bef2bd'>-</i>" + "50000 - 59999 USD" +
-            "<br><i style='color:#14c7a9'>-</i>" + "60000 - 75000 USD" +
-            "<br><i style='color:#fbc95f'>-</i>" + "75000 - 99999 USD" +
-            "<br><i style='color:#fd6137'>-</i>" + "> 100000 USD"
+            "<br><i style='color:#e9bdc6'>--  </i>" + "< 20000 USD" +
+            "<br><i style='color:#ed80ce'>--  </i>" + "20000 - 34999 USD" +
+            "<br><i style='color:#c7a7fa'>--  </i>" + "35000 - 49999 USD" +
+            "<br><i style='color:#bef2bd'>--  </i>" + "50000 - 59999 USD" +
+            "<br><i style='color:#14c7a9'>--  </i>" + "60000 - 75000 USD" +
+            "<br><i style='color:#fbc95f'>--  </i>" + "75000 - 99999 USD" +
+            "<br><i style='color:#fd6137'>--  </i>" + "> 100000 USD"
         );
     };
     info.addTo(map);
